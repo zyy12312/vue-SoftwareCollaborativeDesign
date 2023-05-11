416d266ee4ea926ab4aa4beb7ec5464802e7c62d
lain <2836969767@qq.com>
2023-05-11 09:55:47 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+<!--      <FileUpload></FileUpload>-->
+      <NavBar></NavBar>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+// import FileUpload from "@/components/FileUpload.vue";
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  name: 'App',
+  components: {
+      NavBar,
+      // FileUpload,
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /*margin-top: 60px;*/
+}
+</style>
---
+++ src/components/SubmitWindow.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="submit">
+      <el-form :label-position:="top"  label-width="80px" >
+          <el-form-item label=""></el-form-item>
+          <FileUpload></FileUpload>
+      </el-form>
+  </div>
+</template>
+
+<script>
+// import FileUpload from "@/components/FileUpload.vue";
+
+export default {
+    name: "SubmitWindow"
+}
+</script>
+
+<style scoped>
+
+</style>

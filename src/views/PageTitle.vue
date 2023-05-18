95ad7474484e709a1961788dc8c514c8c2b7e466
15112084645 <1814673259@qq.com>
2023-05-18 22:02:29 +0800

    from zyy 5.18 PageTitle修复

--- src/views/PageTitle.vue
+++ src/views/PageTitle.vue
@@ -5,15 +5,15 @@
                 <div class="grid-content bg-purple-dark" >
                         <p  style="color: black;font-size: 20px;text-align: left">软件协同设计课程网站</p>
                         <p  style="color: #8f8c8c;font-size: 15px;text-align: left">2022-2023 2022-2023第二学期 计算机系-计算机科学教研室</p>
-<!--                    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">-->
-<!--                        <el-menu-item style="text-align: left">授课教师<el-avatar shape="circle" size="large" src="assets/logo.png" /></el-menu-item>-->
-<!--                        <el-menu-item index="1" ><i class="el-icon-s-custom"></i><a href="https://www.ele.me" target="_blank" >班级成员</a></el-menu-item>-->
-<!--                        <el-menu-item index="2" ><i class="el-icon-star-off"></i><a href="https://www.ele.me" target="_blank" >课程信息</a></el-menu-item>-->
-<!--                        <el-menu-item index="3" ><i class="el-icon-tickets"></i><a href="https://www.ele.me" target="_blank" >点名记录</a></el-menu-item>-->
-<!--                        <el-menu-item index="4" ><i class="el-icon-news"></i><a href="https://www.ele.me" target="_blank" >课堂报告</a></el-menu-item>-->
-<!--                        <el-menu-item index="5" ><i class="el-icon-s-marketing"></i><a href="https://www.ele.me" target="_blank" >成绩</a></el-menu-item>-->
-<!--                        <el-menu-item index="6" ><i class="el-icon-pie-chart"></i><a href="https://www.ele.me" target="_blank" >学习分析</a></el-menu-item>-->
-<!--                    </el-menu>-->
+                    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+                        <el-menu-item style="text-align: left">授课教师<el-avatar shape="circle" size="large" src="assets/logo.png" /></el-menu-item>
+                        <el-menu-item index="1" ><i class="el-icon-s-custom"></i><a href="https://www.ele.me" target="_blank" >班级成员</a></el-menu-item>
+                        <el-menu-item index="2" ><i class="el-icon-star-off"></i><a href="https://www.ele.me" target="_blank" >课程信息</a></el-menu-item>
+                        <el-menu-item index="3" ><i class="el-icon-tickets"></i><a href="https://www.ele.me" target="_blank" >点名记录</a></el-menu-item>
+                        <el-menu-item index="4" ><i class="el-icon-news"></i><a href="https://www.ele.me" target="_blank" >课堂报告</a></el-menu-item>
+                        <el-menu-item index="5" ><i class="el-icon-s-marketing"></i><a href="https://www.ele.me" target="_blank" >成绩</a></el-menu-item>
+                        <el-menu-item index="6" ><i class="el-icon-pie-chart"></i><a href="https://www.ele.me" target="_blank" >学习分析</a></el-menu-item>
+                    </el-menu>
                 </div>
             </el-col>
             <el-col >

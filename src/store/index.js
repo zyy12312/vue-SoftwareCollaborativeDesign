1519fed46ca6386a10b68efacef13c4fbe200afa
lain <2836969767@qq.com>
2023-05-21 11:30:14 +0800

    add permission control

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import getters from '../api/getters'
+import getters from './getters'
 import app from "@/App.vue";
-import user from "@/modules/user";
+import user from "@/store/modules/user";
 
 
 Vue.use(Vuex)

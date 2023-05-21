054297e7ba8e237622e049480b48896ecb365c4b
15112084645 <1814673259@qq.com>
2023-05-21 21:49:02 +0800

    app.vue的改动，不需要Push

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <template>
   <div id="app">
-<!--      <NavBar></NavBar>-->
+      <NavBar></NavBar>
 <!--      <AllNavigation></AllNavigation>-->
-<!--      <PageTitle></PageTitle>-->
-<!--      <NavFoot></NavFoot>-->
-      <router-view></router-view>
+      <PageTitle></PageTitle>
+      <NavFoot></NavFoot>
+<!--      <router-view></router-view>-->
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 // import FileUpload from "@/components/FileUpload.vue";
-// import NavBar from "@/views/NavBar.vue";
-// import NavFoot from "@/views/NavFoot.vue";
+import NavBar from "@/views/NavBar.vue";
+import NavFoot from "@/views/NavFoot.vue";
 // import AllNavigation from "@/views/AllNavigation.vue";
-// import PageTitle from "@/views/PageTitle.vue";
+import PageTitle from "@/views/PageTitle.vue";
 //import StudyDiscuss from "@/components/StudyDiscuss.vue";
+
 
 export default {
   name: 'App',
   components: {
+      PageTitle,
+      NavFoot,
+      NavBar
       // PageTitle,
       // AllNavigation,
       //StudyDiscuss,

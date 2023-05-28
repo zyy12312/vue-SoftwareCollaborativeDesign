64feb61485a866f82de43a0c8f09bd0488d3c752
lain <2836969767@qq.com>
2023-05-28 22:29:46 +0800

    add file options

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,6 @@ import Vuex from 'vuex'
 import getters from './getters'
 import app from "@/App.vue";
 import user from "@/store/modules/user";
-import character from "@/store/modules/character";
 
 
 Vue.use(Vuex)
@@ -12,7 +11,6 @@ const store = new Vuex.Store({
     modules: {
         app,
         user,
-        character
     },
     getters
 })
